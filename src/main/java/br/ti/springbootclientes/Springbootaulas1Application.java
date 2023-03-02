3af0fcd93c7000bb299e8b8e10838272e5c91e14
lucasvitor1994 <55961366+lucasvitor1994@users.noreply.github.com>
2023-03-02 13:44:03 -0300

    primeiro commit

---
+++ src/main/java/br/ti/springbootclientes/Springbootaulas1Application.java
@@ -0,0 +1,13 @@
+package br.ti.springbootclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootaulas1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootaulas1Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/ti/springbootclientes/Springbootaulas1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ti.springbootclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootaulas1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
